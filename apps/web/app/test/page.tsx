99f6ae9ee49840dcf5f971ed2ad60fda9e95dfaa
Kunal Dubey <kunal.dubey@dazn.com>
2024-05-15 19:43:46 +0530

    Able to import and export React Library

---
+++ apps/web/app/test/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Experiment } from "@repo/abfy/Experiment";
+import { Variant } from "@repo/abfy/Variant";
+
+function test() {
+  return (
+    <>
+      <h1>Testing</h1>
+      <Experiment />
+      <Variant />
+    </>
+  );
+}
+
+export default test;
