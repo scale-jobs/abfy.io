d0379150c885f85987ad082de35215a90d6eeab7
Kunal Dubey <mailme@kunaldubey.com>
2024-06-29 18:28:57 +0530

    Added Test Coverage

--- packages/abfy/jest.config.js
+++ packages/abfy/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["./jest.setup.ts"],
+  collectCoverage: true,
+  coverageDirectory: "../../coverage",
+  coverageReporters: ["json-summary", "text", "lcov"],
 };
